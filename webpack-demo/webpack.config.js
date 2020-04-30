edddd1ffa9a1294c3edffc4b12d8d98348ad6004
Keenan D'spain <Keenan.dspain@tcu.edu>
2020-04-29 20:03:02 -0500

    committing development for problem 1

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,23 +1,26 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: {
     app: "./src/index.js",
     print: "./src/print.js",
   },
+     devtool: 'inline-source-map',
+     devServer: {
+             contentBase: './dist',
+           },
   plugins: [
-      
-         new HtmlWebpackPlugin({
-           title: 'Output Management',
-         }),
-       ],
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+    }),
+  ],
   mode: "development",
   output: {
-    filename: '[name].bundle.js',
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    publicPath: '/',
   },
 };
