8502d5a66748e125a3cab8e60dce481756d706b5
Keenan D'spain <Keenan.dspain@tcu.edu>
2020-05-01 22:39:34 -0500

    getting started commit

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,35 +1,9 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
 
 module.exports = {
-  entry: {
-    app: "./src/index.js",
-  },
-     devtool: 'inline-source-map',
-     devServer: {
-             contentBase: './dist',
-             hot: true,
-             
-           },
-           module: {
-                 rules: [
-                   {
-                     test: /\.css$/,
-                     use: ['style-loader', 'css-loader'],
-                   },
-                 ],
-               },
-  plugins: [
-    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
-    new HtmlWebpackPlugin({
-      title: "Output Management",
-    }),
-  ],
-  mode: "development",
+  entry: './src/index.js',
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: '/',
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
 };
